a20c581afeb48a3c672395b8bd2bd70c6f3dea6c
Ahmad Awad <alex.awad.s5@gmail.com>
2026-08-15 12:43:50 -0400

    Feature/alex/athan system01 (#1)
    * Read me
    
    * Initial Athan system setup
    
    * initial version
    
    * update wav file
    
    * Update
    
    * Fix touch issue
    
    * Fix bug with touch

---
+++ run_athan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Activate virtual environment
+source /home/alexawad/AthanSystem/AthanSystem/venv/bin/activate
+
+# Set display to Pi screen 0
+export DISPLAY=:0
+
+# Run your Kivy app
+/home/alexawad/AthanSystem/AthanSystem/venv/bin/python /home/alexawad/AthanSystem/AthanSystem/main.py
+
